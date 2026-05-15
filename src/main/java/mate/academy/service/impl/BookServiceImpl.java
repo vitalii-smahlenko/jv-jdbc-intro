4deb731777b2b23e4c516c86e5845756f15bd3eb
vitalii-smahlenko <smaglenko@gmail.com>
2026-05-15 15:28:41 +0300

    checkstyle

--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -21,7 +21,8 @@ public class BookServiceImpl implements BookService {
     @Override
     public Book get(Long id) {
         return bookDao.findById(id).orElseThrow(
-                () -> new DataProcessingException("Can't get book by id " + id, new RuntimeException()));
+                () -> new DataProcessingException(
+                        "Can't get book by id " + id, new RuntimeException()));
     }
 
     @Override
